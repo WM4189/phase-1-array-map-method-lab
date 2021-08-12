a91942c23e563015e668ac6660b6170ce074215f
William Mosto <will.mosto@gmail.com>
2021-08-12 19:18:06 -0400

    commit tree 198c869b79d65b157d0c6be87bac736e7a634e0c

--- index.js
+++ index.js
@@ -12,5 +12,24 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const title = tutorials.map(function (name) {
+    const words = name.split(' ');
+    console.log(words)
+    const combine = words.map(word => word[0].toUpperCase() + word.slice(1));
+    const cased = combine.join(' ');
+    return cased;
+  });
+  return title
 }
+console.log(titleCased());
+
+
+/////////////////////////////////////////////////////////////////
+// let arrayToLongString = tutorials.toString()  // Array => One Long String
+// // console.log(arrayToLongString)
+// let singleWordsArray = arrayToLongString.split(' ') // String => Array of Individual words 
+// // console.log(singleWordsArray)
+
+
+// const titleCased = singleWordsArray.map(x => x.substring(0, 1) + x.toUpperCase())
+// console.log(titleCased)
